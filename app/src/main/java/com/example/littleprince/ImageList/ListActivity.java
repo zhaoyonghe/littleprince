fed877d9cc9dbf1dabef68f3fda9976953e863ad
zhaoyonghe <909672941@qq.com>
2018-05-23 00:30:20 +0800

    加了顶栏图标

--- app/src/main/java/com/example/littleprince/ImageList/ListActivity.java
+++ app/src/main/java/com/example/littleprince/ImageList/ListActivity.java
@@ -8,14 +8,20 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.example.littleprince.BaseActivity;
 import com.example.littleprince.EditImageActivity;
 import com.example.littleprince.Notification.MyNotificationManager;
+import com.example.littleprince.R;
 import com.example.littleprince.utils.FileUtils;
 
 import java.io.File;
+import java.lang.reflect.Method;
 
 
 /**
@@ -27,11 +33,16 @@ public class ListActivity extends BaseActivity {
 
 
     private static ListActivity listContext;
+    private static String curBucket;
 
 
 
     public static ListActivity getListContext(){
         return listContext;
+    }
+
+    public static String getCurBucket(){
+        return curBucket;
     }
 
     @Override
@@ -40,13 +51,15 @@ public class ListActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
 
         listContext=this;
-        Log.d("fuck",this.toString());
 
         //检测图片载入框架是否导入，若没有，则导入并初始化
         checkInitImageLoader();
 
         //在主界面显示ImagesFragment
-        Fragment imagesfragment = new ImagesFragment();
+        ImagesFragment imagesfragment = new ImagesFragment();
+
+        curBucket=imagesfragment.bucketName;
+
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
         transaction.replace(android.R.id.content, imagesfragment);
@@ -59,11 +72,70 @@ public class ListActivity extends BaseActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menus, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.refresh:
+                refresh();
+                return true;
+            case R.id.bucket_changing:
+                return true;
+            case R.id.aboutme:
+                return true;
+            case R.id.donate:
+                return true;
+            case R.id.setting:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        Log.d("fuck","eat fuck");
+        if (featureId== Window.FEATURE_ACTION_BAR&& menu!=null){
+            if(menu.getClass().getSimpleName().equals("MenuBuilder")){
+                try {
+                    Method m=menu.getClass().getDeclaredMethod("setOptionalIconsVisible",Boolean.TYPE);
+                    m.setAccessible(true);
+                    m.invoke(menu,true);
+                }catch (Exception e){
+
+                }
+            }
+        }
+        return super.onMenuOpened(featureId, menu);
+    }
 
     public void refresh(){
         //在主界面显示ImagesFragment
         ImagesFragment imagesfragment = new ImagesFragment();
-        //imagesfragment.bucketName="Camera";
+
+        imagesfragment.bucketName=curBucket;
+
+        curBucket=imagesfragment.bucketName;
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(android.R.id.content, imagesfragment);
+
+        transaction.commit();
+    }
+
+    public void refresh(String bucketName){
+        //在主界面显示ImagesFragment
+        ImagesFragment imagesfragment = new ImagesFragment();
+
+        imagesfragment.bucketName=bucketName;
+
+        curBucket=imagesfragment.bucketName;
+
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
         transaction.replace(android.R.id.content, imagesfragment);
